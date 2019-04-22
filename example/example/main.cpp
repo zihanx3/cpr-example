d8af73f6c786fba5885e6bb7c2efb5496c663c97
zihanx3 <zihanx3@illinois.edu>
2019-04-22 10:17:02 -0500

    xcode

---
+++ example/example/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  example
+//
+//  Created by Crescent on 4/22/19.
+//  Copyright © 2019 Crescent. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
